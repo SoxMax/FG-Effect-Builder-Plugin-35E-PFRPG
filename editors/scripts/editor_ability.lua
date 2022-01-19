0c738044d48dcfc7416a24cfd10e33ac039bd688
SoxMax <irishsoxmax@gmail.com>
2022-01-19 10:10:46 -0500

    support ability checks

--- editors/scripts/editor_ability.lua
+++ editors/scripts/editor_ability.lua
@@ -1,8 +1,24 @@
-function createEffectString()
+local function createAbilityEffectString()
     local effectString = effect.getStringValue() .. ": " .. effect_modifier.getValue()
     if not effect_bonus_type.isEmpty() then
         effectString = effectString .. " " .. effect_bonus_type.getValue()
     end
-    -- parentcontrol.window.effectDescription.setValue(effectString)
     return effectString
+end
+
+local function createAbilityCheckEffectString()
+    local effectString = "ABL: " .. effect_modifier.getValue() .. " " .. DataCommon.ability_stol[effect.getStringValue()]
+    if not effect_bonus_type.isEmpty() then
+        effectString = effectString .. ", " .. effect_bonus_type.getValue()
+    end
+    return effectString
+end
+
+
+function createEffectString()
+    if check_bonus.getValue() == 0 then
+        return createAbilityEffectString()
+    else
+        return createAbilityCheckEffectString()
+    end
 end
