fe9db07b25327f6d833e0fc77cc84f6fc4fdeedf
SoxMax <irishsoxmax@gmail.com>
2022-01-26 17:15:10 -0500

    break ability checks to its own editor

--- editors/effect/scripts/ABILITY.lua
+++ editors/effect/scripts/ABILITY.lua
@@ -1,24 +1,7 @@
-local function createAbilityEffectString()
+function createEffectString()
     local effectString = parentcontrol.window.effect.getStringValue() .. ": " .. number_value.getStringValue()
     if not effect_bonus_type.isEmpty() then
         effectString = effectString .. " " .. effect_bonus_type.getValue()
     end
     return effectString
-end
-
-local function createAbilityCheckEffectString()
-    local effectString = "ABL: " .. number_value.getStringValue() .. " " .. DataCommon.ability_stol[parentcontrol.window.effect.getStringValue()]
-    if not effect_bonus_type.isEmpty() then
-        effectString = effectString .. ", " .. effect_bonus_type.getValue()
-    end
-    return effectString
-end
-
-
-function createEffectString()
-    if check_bonus.getValue() == 0 then
-        return createAbilityEffectString()
-    else
-        return createAbilityCheckEffectString()
-    end
 end
