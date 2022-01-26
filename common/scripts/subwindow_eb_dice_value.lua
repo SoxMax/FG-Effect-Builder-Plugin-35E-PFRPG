931a497a251aadeee5c0b52595d533ee17965d65
SoxMax <irishsoxmax@gmail.com>
2022-01-26 10:19:58 -0500

    fix how space is added

--- common/scripts/subwindow_eb_dice_value.lua
+++ common/scripts/subwindow_eb_dice_value.lua
@@ -3,7 +3,10 @@ function getStringValue()
     local valueString = StringManager.convertDiceToString(dice, effect_modifier.getValue())
     local stat = stat_value.getStringValue()
     if stat ~= "" then
-        valueString = valueString .. " " .. stat
+        if valueString ~= "" then
+            valueString = valueString .. " "
+        end
+        valueString = valueString .. stat
     end
     return valueString
 end
