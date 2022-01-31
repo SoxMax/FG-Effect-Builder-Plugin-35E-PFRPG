0f42e20c876bceeecfdc5ed814f229e075a5f106
SoxMax <irishsoxmax@gmail.com>
2022-01-31 06:09:23 -0800

    Allow damage fields to specify an arbitrary number of damage types (#3)
    * dmg updated with multi damage selectors
    
    * dynamically get the width
    
    * update dmgo and dmgtype with multiple damage types

--- editors/effect/scripts/DMGO.lua
+++ editors/effect/scripts/DMGO.lua
@@ -1,12 +1,8 @@
 function createEffectString()
     local effectString = parentcontrol.window.effect.getStringValue() .. ": " .. dice_value.getStringValue()
-    local descriptors = {}
-    if not damage_type.isEmpty() then
-        table.insert(descriptors, damage_type.getValue())
-    end
-
-    if next(descriptors) then
-        effectString = effectString .. " " .. table.concat(descriptors, ",")
+    local damageType = damage_types.getStringValue()
+    if damageType ~= "" then
+        effectString = effectString .. " " .. damageType
     end
 
     return effectString
