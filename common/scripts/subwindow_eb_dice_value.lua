a47a3464d98aaf3f1e9f56dc7a449eb4c6893d4b
SoxMax <irishsoxmax@gmail.com>
2022-03-04 16:50:48 -0500

    update to use DiceManager

--- common/scripts/subwindow_eb_dice_value.lua
+++ common/scripts/subwindow_eb_dice_value.lua
@@ -1,6 +1,6 @@
 function getStringValue()
     local dice = effect_dice and effect_dice.getDice()
-    local valueString = StringManager.convertDiceToString(dice, effect_modifier.getValue())
+    local valueString = DiceManager.convertDiceToString(dice, effect_modifier.getValue())
     local stat = stat_value.getStringValue()
     if stat ~= "" then
         if valueString ~= "" then
