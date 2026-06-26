464ba2508d1b0e220dc53f28b76b69455b9d2fc8
SoxMax <irishsoxmax@gmail.com>
2026-06-26 12:50:36 -0400

    visual toggle

--- editors/effect/scripts/SIZE.lua
+++ editors/effect/scripts/SIZE.lua
@@ -1,3 +1,13 @@
+function onInit()
+    updateSizeMode()
+end
+
+function updateSizeMode()
+    local isStep = size_mode.getStringValue() == "Step"
+    number_value.setVisible(isStep)
+    size.setVisible(not isStep)
+end
+
 function createEffectString()
     local base = parentcontrol.window.effect.getStringValue() .. ": "
     if size_mode.getStringValue() == "Step" then
